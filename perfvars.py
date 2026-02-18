ff131cb3177a56b1af2e36b3af19587ebddd7777
David Gravereaux <davygrvy@pobox.com>
2026-02-18 13:47:05 -0800

    simplistic caching added to avoid duplicated db requests

--- perfvars.py
+++ perfvars.py
@@ -10,11 +10,60 @@ class Count:
   def val(self):
    return self.a
 
+release_group_cache = dict()
+release_cache = dict()
+event_cache = dict()
+place_cache = dict()
+area_cache = dict()
+
+def get_release_group (mbid):
+    if mbid in release_group_cache:
+        return release_group_cache[mbid]
+    else:
+        release_group = musicbrainzngs.get_release_group_by_id(mbid,
+                includes=['event-rels','place-rels','area-rels'])['release-group']
+        release_group_cache[mbid] = release_group
+        return release_group
+
+def get_release (mbid):
+    if mbid in release_cache:
+        return release_cache[mbid]
+    else:
+        release = musicbrainzngs.get_release_by_id(mbid,
+            includes=['release-groups','event-rels','place-rels','area-rels'])['release']
+        release_cache[mbid] = release
+        return release
+
+def get_event (mbid):
+    if mbid in event_cache:
+        return event_cache[mbid]
+    else:
+        event = musicbrainzngs.get_event_by_id(mbid,
+                includes=['place-rels','area-rels'])['event']
+        event_cache[mbid] = event
+        return event
+
+def get_place (mbid):
+    if mbid in place_cache:
+        return place_cache[mbid]
+    else:
+        place = musicbrainzngs.get_place_by_id(mbid,
+                includes=['place-rels','area-rels'])['place']
+        event_cache[mbid] = event
+        return place
+
+def get_area (mbid):
+    if mbid in area_cache:
+        return area_cache[mbid]
+    else:
+        area = musicbrainzngs.get_area_by_id(mbid,
+                includes=["area-rels"])['area']
+        area_cache[mbid] = area
+        return area
 
 def add_album_performance_metadata(metadata, release_mbid):
     # re-get the release with our special includes
-    release = musicbrainzngs.get_release_by_id(release_mbid,
-            includes=['release-groups','event-rels','place-rels','area-rels'])['release']
+    release = get_release(release_mbid)
     
     count = Count()
     just_once = 1
@@ -27,8 +76,7 @@ def add_album_performance_metadata(metadata, release_mbid):
             processEventRelations(release['event-relation-list'], metadata, count)
             break
         
-        release_group = musicbrainzngs.get_release_group_by_id(release['release-group']['id'],
-                includes=['event-rels','place-rels','area-rels'])['release-group']
+        release_group = get_release_group(release['release-group']['id'])
     
         # check if there is an event relationships on the release-group
         # we do this second as release events take precidence
@@ -76,8 +124,7 @@ def processEventRelations(event_relation_list, metadata, count):
                 metadata[f"~release_performance{count.val()}_time"] = event_rel['event']['time']
                 
             # now get the event for a deeper look
-            event = musicbrainzngs.get_event_by_id(event_rel['event']['id'],
-                    includes=['place-rels','area-rels'])['event']
+            event = get_event(event_rel['event']['id'])
             
             if 'place-relation-list' in event:
                 processPlaceRelations(event['place-relation-list'], metadata, count)
@@ -88,35 +135,41 @@ def processEventRelations(event_relation_list, metadata, count):
 
 
 def processPlaceRelations(place_relation_list, metadata, count):
-    for place_rel in place_relation_list:
-        if place_rel['type-id'] == 'e2c6f697-07dc-38b1-be0b-83d740165532':
+    for place_rel in place_relation_list:        
+        if place_rel['type-id'] in [
+                'e2c6f697-07dc-38b1-be0b-83d740165532',
+                '3b1fae9f-5b22-42c5-a40c-d1e5c9b90251',
+                'a64a9085-505b-4588-bff9-214d7dda61c4']:
             # 'held at' on an event https://musicbrainz.org/relationship/e2c6f697-07dc-38b1-be0b-83d740165532
-            if 'target-credit' in place_rel:
-                metadata[f"~release_performance{count.val()}_location"] = place_rel['target-credit']
-                metadata[f"~release_performance{count.val()}_location_unwound"] = (
-                        ", ".join([place_rel['target-credit'], unwindPlace(place_rel['place']['id'])[1]]))
-            else:
-                metadata[f"~release_performance{count.val()}_location"] = place_rel['place']['name']
-                metadata[f"~release_performance{count.val()}_location_unwound"] = (
-                        ", ".join(unwindPlace(place_rel['place']['id'])))
-            count.incr()
-            continue
-        
-        if place_rel['type-id'] in ['3b1fae9f-5b22-42c5-a40c-d1e5c9b90251','a64a9085-505b-4588-bff9-214d7dda61c4']:
             # 'recorded at' on a release https://musicbrainz.org/relationship/3b1fae9f-5b22-42c5-a40c-d1e5c9b90251
             # 'recorded at' on a release-group https://musicbrainz.org/relationship/a64a9085-505b-4588-bff9-214d7dda61c4
-            if place_rel['begin'] == place_rel['end']:
-                metadata[f"~release_performance{count.val()}_date"] = place_rel['begin']
-            else:
-                metadata[f"~release_performance{count.val()}_date"] = f"{place_rel['begin']} - {place_rel['end']}"
+            
+            place = get_place(place_rel['place']['id'])
+            
+            # places from events won't have a date
+            if 'begin' in place_rel:
+                if place_rel['begin'] == place_rel['end']:
+                    metadata[f"~release_performance{count.val()}_date"] = place_rel['begin']
+                else:
+                    metadata[f"~release_performance{count.val()}_date"] = (
+                            f"{place_rel['begin']} - {place_rel['end']}")
+            
+            if 'address' in place:
+                metadata[f"~release_performance{count.val()}_location_addr"] = place['address']
+            
+            if 'coordinates' in place:
+                metadata[f"~release_performance{count.val()}_location_gps"] = (
+                        f"{place['coordinates']['latitude']},{place['coordinates']['longitude']}")
+            
             if 'target-credit' in place_rel:
                 metadata[f"~release_performance{count.val()}_location"] = place_rel['target-credit']
                 metadata[f"~release_performance{count.val()}_location_unwound"] = (
-                        ", ".join([place_rel['target-credit'], unwindPlace(place_rel['place']['id'])[1]]))
+                        ", ".join([place_rel['target-credit'], unwindPlace(place=place)[1]]))
             else:
                 metadata[f"~release_performance{count.val()}_location"] = place_rel['place']['name']
                 metadata[f"~release_performance{count.val()}_location_unwound"] = (
-                        ", ".join(unwindPlace(place_rel['place']['id'])))
+                        ", ".join(unwindPlace(place=place)))
+            
             count.incr()
 
 
@@ -139,9 +192,9 @@ def processAreaRelations(area_relation_list, metadata, count):
                         ", ".join(unwindArea(area_rel['area']['id'])))
             count.incr()
 
-
-def unwindPlace(mbid):
-    place = musicbrainzngs.get_place_by_id(mbid, includes=['place-rels','area-rels'])['place']
+def unwindPlace(mbid="", place=""):
+    if not place:
+        place = get_place(mbid)
     if 'place-relation-list' in place:
         for place_rel in place['place-relation-list']:
             if place_rel['direction'] == 'backward' and place_rel['type-id'] == 'ff683f48-eff1-40ab-a58f-b128098ffe92':
@@ -153,7 +206,7 @@ def unwindPlace(mbid):
 
 def unwindArea(mbid):
     # town/city, district, county, municipality, subdivision (state), country
-    area = musicbrainzngs.get_area_by_id(mbid, includes=["area-rels"])['area']
+    area = get_area(mbid)
     if 'area-relation-list' in area:
        for area_rel in area['area-relation-list']:
           if (area_rel['direction'] == 'backward' and 
