d38ce300763c8c8f385f3a5131173bb5a59f69a4
David Gravereaux <davygrvy@pobox.com>
2026-02-19 11:46:25 -0800

    clear_cache () added so we can hook this into a context menu item

--- cache.py
+++ cache.py
@@ -1,4 +1,4 @@
-# TODO: use musicbrainzngs for now, but redo for tagger.webservice
+# TODO: use musicbrainzngs for now, but redo for album.tagger.webservice
 import musicbrainzngs
 musicbrainzngs.set_useragent("perfvars plugin dev", "0.1", "davygrvy@pobox.com")
 musicbrainzngs.set_rate_limit(0.5, 2)
@@ -9,7 +9,20 @@ event_cache = dict()
 place_cache = dict()
 area_cache = dict()
 
+
+def clear_cache ():
+    global release_group_cache,release_cache,event_cache,place_cache,area_cache
+    del release_group_cache,release_cache,event_cache,place_cache,area_cache
+    
+    release_group_cache = dict()
+    release_cache = dict()
+    event_cache = dict()
+    place_cache = dict()
+    area_cache = dict()
+    
+
 # try to be somewhat kind to our server while we abuse it
+
 def get_release_group (mbid):
     if mbid in release_group_cache:
         return release_group_cache[mbid]
@@ -42,7 +55,7 @@ def get_place (mbid):
         return place_cache[mbid]
     else:
         place = musicbrainzngs.get_place_by_id(mbid,
-                includes=['place-rels','area-rels'])['place']
+                includes=['annotation','place-rels','area-rels'])['place']
         event_cache[mbid] = place
         return place
 
@@ -54,4 +67,3 @@ def get_area (mbid):
                 includes=["area-rels"])['area']
         area_cache[mbid] = area
         return area
-
